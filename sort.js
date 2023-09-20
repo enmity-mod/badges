c776c2b0c27eba0953330f7fb658147ed0d5edda
eternal <eternal.git@gmail.com>
2023-09-20 17:51:45 +0100

    update patrons

--- sort.js
+++ sort.js
@@ -4,20 +4,20 @@ const users = fs.readdirSync('.').filter(e => e.endsWith('.json'));
 const order = ['dev', 'staff', 'contributor', 'supporter'];
 
 for (const user of users) {
-   const content = fs.readFileSync(user, 'utf-8');
-   const badges = JSON.parse(content);
+	const content = fs.readFileSync(user, 'utf-8');
+	const badges = JSON.parse(content);
 
-   // Sort badges in a specific order
-   const sorted = badges.sort((a, b) => {
-      if (!Number.isNaN(Number(a))) {
-         return 1;
-      } else if (!Number.isNaN(Number(b))) {
-         return -1;
-      }
+	// Sort badges in a specific order
+	const sorted = badges.sort((a, b) => {
+		if (/\d/.test(a)) {
+			return 1;
+		} else if (/\d/.test(b)) {
+			return -1;
+		}
 
-      return order.indexOf(a) - order.indexOf(b);
-   });
+		return order.indexOf(a) - order.indexOf(b);
+	});
 
-   fs.writeFileSync(user, JSON.stringify(sorted), 'utf-8');
-   console.log(`Sorted ${user}`);
+	fs.writeFileSync(user, JSON.stringify(sorted), 'utf-8');
+	console.log(`Sorted ${user}`);
 }
